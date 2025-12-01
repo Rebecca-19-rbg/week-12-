c0cc4ed08ef5b2ba25b669666629626277355f07
Rebecca-19-rbg <rclove1@cps.edu>
2025-12-01 08:46:02 -0600

    Update 4 — Nested Lists and List Comprehensions.py

--- week 13  csp/4 — Nested Lists and List Comprehensions.py
+++ week 13  csp/4 — Nested Lists and List Comprehensions.py
@@ -60,12 +60,24 @@ for row in num_pad:
 # Practice Problems:
 
 # Build a matrix variable containing 3 lists of 3 numbers each.
-#matrix_examp = ()
+list1 = (3, 2, 1)
+list2 = (6, 5, 4)
+list3 = (9, 8, 7)
+
+matrix_examp = (list1, list2, list3)
+
 
 # Print the first list.
+print(matrix_examp[0])
 
 # Print the second item from the third list.
+print(matrix_examp[2][1])
 
 # Use a list comprehension to extract the last item from each sub-list.
+last_item = [row[2] for row in matrix_examp]
+print(last_item)
 
 # Challenge: Create a new list containing squares of numbers from 1–10 using a comprehension.
+
+squares = [x**2 for x in range (1,11)]
+print(squares)
